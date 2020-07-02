e5c5802da2b26a2f5adfc6225d76019f10eac186
Lukas Spies <LSpies@illinois.edu>
2020-07-01 22:09:27 -0500

    add host-to-device memcpy direction, run both in succession and write to different files

--- gs/memory_copy.cpp
+++ gs/memory_copy.cpp
@@ -15,34 +15,75 @@ int deviceMemcpyTest(occa::device device) {
     time(&rawtime);
     timeinfo = localtime(&rawtime);
 
-    printf("Starting memcpy measurement... ");
-    fflush(stdout);
+    {
 
-    strftime(buffer,80,"device_memcpy_%Y_%m_%d_%R.txt", timeinfo);
-    FILE *fp = fopen(buffer, "w");
-    fprintf(fp, "%-10s %-10s\n", "words", "timing");
+        printf("Starting device-to-host memcpy measurement... ");
+        fflush(stdout);
 
-    for(int size = min_message_size; size <= max_message_size; size=((int)(size*1.1) > size ? (int)(size*1.1) : size+1)) {
+        strftime(buffer,80,"memcpy_devicetohost_%Y_%m_%d_%R.txt", timeinfo);
+        FILE *fp = fopen(buffer, "w");
+        fprintf(fp, "%-10s %-10s\n", "words", "timing");
 
-        occa::memory d_buf = device.malloc(size*sizeof(double));
-        double *h_buf = new double[size];
+        for(int size = min_message_size; size <= max_message_size; size=((int)(size*1.1) > size ? (int)(size*1.1) : size+1)) {
 
-        // warmup
-        for(int i = 0; i < 10; ++i)
-            d_buf.copyTo(h_buf, size*sizeof(double), 0);
+            occa::memory d_buf = device.malloc(size*sizeof(double));
+            double *h_buf = new double[size];
 
-        auto t_start = std::chrono::steady_clock::now();
-        for(int i = 0; i < count; ++i)
-            d_buf.copyTo(h_buf, size*sizeof(double), 0);
-        auto t_end = std::chrono::steady_clock::now();
+            // warmup
+            for(int i = 0; i < 10; ++i)
+                d_buf.copyTo(h_buf, size*sizeof(double), 0);
 
-        double timing = std::chrono::duration<double, std::milli>(t_end-t_start).count();
+            auto t_start = std::chrono::steady_clock::now();
+            for(int i = 0; i < count; ++i)
+                d_buf.copyTo(h_buf, size*sizeof(double), 0);
+            auto t_end = std::chrono::steady_clock::now();
 
-        fprintf(fp, "%-10d %-10f\n", size, timing/(double)count);
+            double timing = std::chrono::duration<double, std::milli>(t_end-t_start).count();
+
+            fprintf(fp, "%-10d %-10f\n", size, timing/(double)count);
+
+            delete[] h_buf;
+
+        }
+
+        printf("done.\n");
 
     }
 
-    printf("done.\n");
+    {
+
+        printf("Starting host-to-device memcpy measurement... ");
+        fflush(stdout);
+
+        strftime(buffer,80,"memcpy_hosttodevice_%Y_%m_%d_%R.txt", timeinfo);
+        FILE *fp = fopen(buffer, "w");
+        fprintf(fp, "%-10s %-10s\n", "words", "timing");
+
+        for(int size = min_message_size; size <= max_message_size; size=((int)(size*1.1) > size ? (int)(size*1.1) : size+1)) {
+
+            occa::memory d_buf = device.malloc(size*sizeof(double));
+            double *h_buf = new double[size]{};
+
+            // warmup
+            for(int i = 0; i < 10; ++i)
+                d_buf.copyFrom(h_buf, size*sizeof(double), 0);
+
+            auto t_start = std::chrono::steady_clock::now();
+            for(int i = 0; i < count; ++i)
+                d_buf.copyFrom(h_buf, size*sizeof(double), 0);
+            auto t_end = std::chrono::steady_clock::now();
+
+            double timing = std::chrono::duration<double, std::milli>(t_end-t_start).count();
+
+            fprintf(fp, "%-10d %-10f\n", size, timing/(double)count);
+
+            delete[] h_buf;
+
+        }
+
+        printf("done.\n");
+
+    }
 
     return EXIT_SUCCESS;
 
